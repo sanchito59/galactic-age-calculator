0f452f5d95b9aab391574960347b2acc291678a4
Chris Sanchez <c.sanch7@gmail.com>
2019-11-15 15:04:26 -0800

    clean up and import from galacticAge.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import $ from 'jquery';
+import { Age } from './../src/galacticAge.js'
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
-$(document).ready(function(){
+$(document).ready(function () {
   console.log("hello");
-
+  console.log(Age);
 });
