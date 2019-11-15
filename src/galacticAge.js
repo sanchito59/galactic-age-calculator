6d89fad86cfe65139ad2741b1735bacb6b42ce5e
Chris Sanchez <c.sanch7@gmail.com>
2019-11-15 15:05:18 -0800

    clean up semi-colons warned about during 'npm run build'

--- src/galacticAge.js
+++ src/galacticAge.js
@@ -13,24 +13,24 @@ export class Age {
     mercuryYears() {
         const mercuryAge = (this.age / 0.24).toFixed(2);
         return mercuryAge;
-    };
+    }
     venusYears() {
         const venusAge = (this.age / 0.62).toFixed(2);
         return venusAge;
-    };
+    }
     martianYears() {
         const martianAge = (this.age / 1.88).toFixed(2);
         return martianAge;
-    };
+    }
     jupiterYears() {
         const jupiterAge = (this.age / 11.88).toFixed(2);
         return jupiterAge;
-    };
+    }
     lifeExpectancy() {
         const averageLife = 79.6;
         const lifeLeft = (averageLife - this.age).toFixed(2);
         return lifeLeft;
-    };
+    }
     mercuryLifeExpectancy() {
         const averageLife = 79.6;
         const lifeLeft = (averageLife - (this.age / 0.24)).toFixed(2);
@@ -39,7 +39,7 @@ export class Age {
         } else if (lifeLeft > 0) {
             return "You have " + lifeLeft + " years left to live on Mercury based on the average life expectancy in Oregon.";
         }
-    };
+    }
     venusLifeExpectancy() {
         const averageLife = 79.6;
         const lifeLeft = (averageLife - (this.age / 0.62)).toFixed(2);
@@ -48,7 +48,7 @@ export class Age {
         } else if (lifeLeft > 0) {
             return "You have " + lifeLeft + " years left to live on Venus based on the average life expectancy in Oregon.";
         }
-    };
+    }
     martianLifeExpectancy() {
         const averageLife = 79.6;
         const lifeLeft = (averageLife - (this.age / 1.88)).toFixed(2);
@@ -67,4 +67,4 @@ export class Age {
             return "You have " + lifeLeft + " years left to live on Mars based on the average life expectancy in Oregon.";
         }
     }
-};
+}
