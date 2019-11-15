21a013a3c5bf9035fd34f1a63ca77a846507e398
Chris Sanchez <c.sanch7@gmail.com>
2019-11-15 08:53:30 -0800

    add second failing test

--- __tests__/galacticAge.test.js
+++ __tests__/galacticAge.test.js
@@ -6,4 +6,8 @@ describe('ageCalculator', () => {
         let ageCalculatorTest = ageCalculator('apple');
         expect(ageCalculatorTest).toBe(false);
     });
+    test('should determine whether an integer is a positive number greater than 0', () => {
+        let ageCalculatorTest = ageCalculator(2);
+        expect(ageCalculatorTest).toEqual(false);
+    })
 });
--- src/galacticAge.js
+++ src/galacticAge.js
@@ -2,5 +2,7 @@ export function ageCalculator(numInput){
     let number = 5;
     if(typeof(numInput) !== typeof(number)){
         return false;
+    } else if (numInput < 1){
+        return false;
     }
 }
